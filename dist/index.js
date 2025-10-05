5fc15f9cbb490b3d2ac0814b4c56b438db69ca22
Wasp-stdnt <st5353320@stud.duikt.edu.ua>
2025-10-05 17:41:00 +0300

    Add the TS code

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const str = "Hello World!";
+const num = 69;
+const bool = true;
+console.log(`String: ${str}
+Number: ${num}
+Boolean: ${bool}`);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const str: string = "Hello World!";
+const num: number = 69;
+const bool: boolean = true;
+
+console.log(`String: ${str}
+Number: ${num}
+Boolean: ${bool}`);
